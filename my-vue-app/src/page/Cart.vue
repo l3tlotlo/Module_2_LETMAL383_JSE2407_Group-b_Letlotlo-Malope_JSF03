fd72edbc3bcbf507683621cd067baf61dc0c55e4
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 01:17:40 +0200

    cart filtering and remove button

--- my-vue-app/src/page/Cart.vue
+++ my-vue-app/src/page/Cart.vue
@@ -26,7 +26,7 @@
           <p class="text-blue-500 font-bold mb-2">${{ product.price.toFixed(2) }}</p>
           <button @click="removeFromCart(product.id)" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">Remove from Cart</button>
         </div>
-      </div>
+      </div> 
     </div>
   </template>
   
