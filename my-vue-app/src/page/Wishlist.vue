d847c862c2828e6aa2a01ec9ff2e570cd93f356a
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 00:58:32 +0200

    styled wishlist head and back button

---
+++ my-vue-app/src/page/Wishlist.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-4 bg-[#caf0f8]">
+      <h1 class="text-2xl font-semibold mb-4">Wishlist</h1>
+      <button @click="$router.push('/')" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition mb-4">Back to Products</button>
+      
+     
+    </div>
+</template>
+  
+ 
