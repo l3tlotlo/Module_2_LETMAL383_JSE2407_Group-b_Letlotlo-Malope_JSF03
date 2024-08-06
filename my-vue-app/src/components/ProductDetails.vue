065f272a63c6a78c7fbd4c7703993054ddec9c29
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 01:38:26 +0200

    added buttons, price, details and description to product details

---
+++ my-vue-app/src/components/ProductDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <button @click="$router.go(-1)">Back</button>
+      <div v-if="product">
+        <h1>{{ product.title }}</h1>
+        <img :src="product.image" :alt="product.title" class="w-full h-auto" />
+        <p>{{ product.description }}</p>
+        <p>{{ product.price }}</p>
+        <button @click="addToCart(product)" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">Add to Cart</button>
+        <button @click="addToWishlist(product)" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition">Add to Wishlist</button>
+        <button @click="removeFromCart(product)" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">Remove from Cart</button>
+        <button @click="removeFromWishlist(product)" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">Remove from Wishlist</button>
+      </div>
+    </div>
+  </template>
