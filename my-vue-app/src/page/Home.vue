82aa2ee5e86d5e26794682e5cc8ff59d35f882c1
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 00:18:28 +0200

    loading spinner set

--- my-vue-app/src/page/Home.vue
+++ my-vue-app/src/page/Home.vue
@@ -17,11 +17,15 @@
         <option value="default">Sort by: Default</option>
         <option value="price-asc">Sort by: Price - Low to High</option>
         <option value="price-desc">Sort by: Price - High to Low</option>
-      </select>
-      
-    </div>
-
-    
+      </select>      
+    </div>    
   </div>
+ 
+  <div v-if="loading" class="flex items-center justify-center">
+      <svg class="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+     </svg>
+ </div>
 </template>
 
