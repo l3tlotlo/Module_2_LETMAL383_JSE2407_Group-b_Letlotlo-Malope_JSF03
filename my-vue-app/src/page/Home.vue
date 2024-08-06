cf398bd73f8f26c7b602425c1261142c28fdc9d3
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 00:47:18 +0200

    Imported List and Login,registered the components

--- my-vue-app/src/page/Home.vue
+++ my-vue-app/src/page/Home.vue
@@ -1,8 +1,32 @@
 <template>
-    <div>
-      <ProductList @select-product="selectProduct" />
-      <Login v-if="showLogin" @login="handleLogin" @cancel="showLogin = false" />
-    </div>
-  </template>
-  
+  <div>
+    <ProductList @select-product="selectProduct" />
+    <Login v-if="showLogin" @login="handleLogin" @cancel="showLogin = false" />
+  </div>
+</template>
 
+<script>
+import ProductList from '../components/ProductList.vue';
+import Login from '../../src/components/Login.vue';
+
+export default {
+  components: {
+    ProductList,
+    Login
+  },
+  data() {
+    return {
+      showLogin: false
+    };
+  },
+  methods: {
+    handleLogin() {
+      this.$store.commit('setIsLoggedIn', true);
+      this.showLogin = false;
+    },
+    selectProduct(product) {
+      this.$router.push({ name: 'ProductDetails', params: { id: product.id } });
+    }
+  }
+};
+</script>
