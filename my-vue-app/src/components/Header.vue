d5fb4626fa84e4d4143a1a13dfecc2083b978e30
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-06 20:55:49 +0200

    navigation links for wishlist, login and cart. cart icon only visible on large screens, login and wishlist always visible

--- my-vue-app/src/components/Header.vue
+++ my-vue-app/src/components/Header.vue
@@ -5,6 +5,11 @@
         <img src="../assets/online-shop.png" class="h-8" alt="SwiftCart Logo" />
         <span class="text-2xl font-semibold text-white">SwiftCart</span>
       </router-link>
+      <button @click="isOpen = !isOpen" type="button" class="text-white md:hidden">
+        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
       
     </nav>
   </header>
