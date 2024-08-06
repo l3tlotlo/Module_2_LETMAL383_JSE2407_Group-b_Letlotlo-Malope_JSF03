4c03eaed75c7ebdb11b60f4eb7f48515f3ee3a01
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-06 22:01:55 +0200

    main.js importing styling, routing and store

---
+++ my-vue-app/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index';
+import './assets/tailwind.css'
+import store from './store/index'; // import Vuex store
+
+
+import process from 'process';
+window.process = process;
+
+createApp(App)
+  .use(router)
+  .use(store) 
+  .mount('#app');
