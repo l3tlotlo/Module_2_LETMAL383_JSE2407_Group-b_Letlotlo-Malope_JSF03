7b87c35614789aa239e60755a88d36b91c1d0ed7
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 09:19:57 +0200

    index.js router JSDocs comments added

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -1,17 +1,41 @@
-
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../../src/page/Home.vue';
 import ProductDetails from '../components/ProductDetails.vue';
 import Cart from '../../src/page/Cart.vue';
 import Wishlist from '../../src/page/Wishlist.vue';
 
+/**
+ * The route definitions for the application.
+ * @type {Array.<{path: string, name: string, component: Object, props?: boolean}>}
+ */
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/product/:id', name: 'ProductDetails', component: ProductDetails, props: true },
-  { path: '/cart', name: 'Cart', component: Cart },
-  { path: '/wishlist', name: 'Wishlist', component: Wishlist }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductDetails',
+    component: ProductDetails,
+    props: true
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/wishlist',
+    name: 'Wishlist',
+    component: Wishlist
+  }
 ];
 
+/**
+ * Creates the Vue Router instance.
+ * @returns {Router} The router instance.
+ */
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
