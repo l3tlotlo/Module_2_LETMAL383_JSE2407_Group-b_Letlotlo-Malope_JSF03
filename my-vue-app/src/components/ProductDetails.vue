ce84e39140ccecc712d1c204d33ecb7362a1fe46
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 09:02:09 +0200

    JSDocs comments added to ProductDetails modal

--- my-vue-app/src/components/ProductDetails.vue
+++ my-vue-app/src/components/ProductDetails.vue
@@ -8,8 +8,8 @@
         <p>{{ product.price }}</p>
         <button @click="addToCart(product)" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">Add to Cart</button>
         <button @click="addToWishlist(product)" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition">Add to Wishlist</button>
-        <button @click="removeFromCart(product)" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">Remove from Cart</button>
-        <button @click="removeFromWishlist(product)" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">Remove from Wishlist</button>
+        <button @click="removeFromCart(product.id)" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">Remove from Cart</button>
+        <button @click="removeFromWishlist(product.id)" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">Remove from Wishlist</button>
       </div>
     </div>
   </template>
@@ -17,27 +17,55 @@
   <script>
   export default {
     computed: {
+      /**
+       * Retrieves the product based on the route parameter `id`.
+       * First, it tries to find the product in the cart items,
+       * then in the wishlist items.
+       * Logs the product to check the image URL.
+       * @returns {Object|null} The product object or null if not found.
+       */
       product() {
         const product = this.$store.getters.cartItems.find(product => product.id === Number(this.$route.params.id)) || 
                         this.$store.getters.wishlistItems.find(product => product.id === Number(this.$route.params.id));
+        console.log('Product:', product); // Log product to check image URL
         return product;
       }
     },
     methods: {
+      /**
+       * Adds a product to the cart.
+       * @param {Object} product - The product to add.
+       */
       addToCart(product) {
         this.$store.commit('cart/addToCart', product);
       },
+      /**
+       * Adds a product to the wishlist.
+       * @param {Object} product - The product to add.
+       */
       addToWishlist(product) {
         this.$store.commit('wishlist/addToWishlist', product);
       },
+      /**
+       * Removes a product from the cart.
+       * @param {number} productId - The ID of the product to remove.
+       */
       removeFromCart(productId) {
         this.$store.commit('cart/removeFromCart', productId);
       },
+      /**
+       * Removes a product from the wishlist.
+       * @param {number} productId - The ID of the product to remove.
+       */
       removeFromWishlist(productId) {
         this.$store.commit('wishlist/removeFromWishlist', productId);
       }
     },
     created() {
+      /**
+       * Fetches products if the product is not found in the store.
+       * Forces the component to update after fetching products.
+       */
       if (!this.product) {
         this.$store.dispatch('fetchProducts').then(() => {
           this.$forceUpdate(); // Force component update after fetch
@@ -46,3 +74,4 @@
     }
   };
   </script>
+  
