c90c628238eab1dd5580f07c2aeaf434b08e28e1
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 09:14:34 +0200

    JSDoc commentsa dded to Home.vue

--- my-vue-app/src/page/Home.vue
+++ my-vue-app/src/page/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <!-- Product list component with event binding for product selection -->
     <ProductList @select-product="selectProduct" />
+    <!-- Login component, conditionally rendered based on showLogin flag -->
     <Login v-if="showLogin" @login="handleLogin" @cancel="showLogin = false" />
   </div>
 </template>
@@ -16,14 +18,26 @@ export default {
   },
   data() {
     return {
+      /**
+       * Flag to show or hide the login component.
+       * @type {Boolean}
+       */
       showLogin: false
     };
   },
   methods: {
+    /**
+     * Handles the login action, setting the user as logged in and hiding the login component.
+     */
     handleLogin() {
       this.$store.commit('setIsLoggedIn', true);
       this.showLogin = false;
     },
+    /**
+     * Handles product selection, navigating to the product details page.
+     * @param {Object} product - The selected product object.
+     * @param {Number} product.id - The ID of the selected product.
+     */
     selectProduct(product) {
       this.$router.push({ name: 'ProductDetails', params: { id: product.id } });
     }
