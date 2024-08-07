23ad3f34c743a24a03dbe928ca5c01b6754a8513
Letlotlo Malope <letlotlomal@gmail.com>
2024-08-07 09:32:48 +0200

    created JSDocs comments for component, data property and method

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,5 +1,3 @@
-App.vue
-
 <template>
   <div id="app">
     <Header @toggle-login="showLogin = !showLogin" />
@@ -12,6 +10,11 @@ App.vue
 import Header from '../src/components/Header.vue';
 import Login from '../src/components/Login.vue';
 
+/**
+ * The root component of the application.
+ * It includes a header, a router view for rendering different routes,
+ * and a login modal that toggles based on user interaction.
+ */
 export default {
   components: {
     Header,
@@ -19,10 +22,20 @@ export default {
   },
   data() {
     return {
+      /**
+       * Controls the visibility of the login modal.
+       * @type {boolean}
+       */
       showLogin: false, 
     };
   },
   methods: {
+    /**
+     * Handles the login action.
+     * This method is called when the login is successful.
+     * It navigates to the '/product-list' route and hides the login modal.
+     * @fires router.push
+     */
     handleLogin() {
       this.$router.push('/product-list'); 
       this.showLogin = false;
